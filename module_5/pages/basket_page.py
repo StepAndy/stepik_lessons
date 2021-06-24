abc3ac72d43db348e868688439ebc04eec630e41
Andrey Stepanov <aastepanov@intermedia.com>
2021-06-24 23:12:50 +0300

    added homework # 5

--- module_5/pages/basket_page.py
+++ module_5/pages/basket_page.py
@@ -4,10 +4,9 @@ from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
     def should_be_no_products_in_basket(self):
-        assert self.is_not_element_present(*BasketPageLocators.PROCEED_TO_CHECKOUT),\
+        assert self.is_not_element_present(*BasketPageLocators.PROCEED_TO_CHECKOUT), \
             "There are products in basket"
 
     def should_be_text_basket_is_empty(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET),\
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET), \
             "Text saying basket is empty was not found"
-
--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -7,11 +7,30 @@ from module_5.pages.basket_page import BasketPage
 
 
 class TestProductPage:
+    @pytest.mark.parametrize('promo_offer',
+                             ["offer0", "offer1", "offer2",
+                              "offer3", "offer4", "offer5",
+                              "offer6", pytest.param("offer7", marks=pytest.mark.xfail),
+                              "offer8", "offer9"])
+    def test_guest_can_add_product_to_basket(self, browser, promo_offer):
+        full_promo_link = ProductPage.LINK + '?promo=' + promo_offer
+        page = ProductPage(browser, full_promo_link)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.check_specific_product_was_added_to_basket()
+        page.check_basket_and_product_costs_are_equal()
+
     @pytest.mark.xfail
     def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
         page = ProductPage(browser, ProductPage.LINK)
         page.open()
         page.add_to_basket()
+        page.should_not_be_success_message()
+
+    def test_guest_cant_see_success_message(self, browser):
+        page = ProductPage(browser, ProductPage.LINK)
+        page.open()
         page.should_not_be_success_message()
 
     @pytest.mark.xfail
